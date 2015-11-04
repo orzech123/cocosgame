fd78f5f19a08e13b4072c363d8a2812f4e01dbdb
MO <michal.orzechowski@caliper.pl>
2015-11-04 21:03:29 +0100

    **

--- src/MenuLayer.js
+++ src/MenuLayer.js
@@ -34,61 +34,29 @@ var MenuLayer = (function (_super) {
             //cc.director.runScene(new PlayScene());
         };
 
-        //2. get the screen size of your game canvas
         var winsize = cc.director.getWinSize();
+        var centerpos = cc.p(winsize.width / 2, winsize.height / 2);
 
         this.setAnchorPoint(0, 0);
         this.setPosition(-winsize.width / 2, -winsize.height / 2);
 
-        //this.init = this.initOverride;
-        //3. calculate the center point
-        var centerpos = cc.p(winsize.width / 2, winsize.height / 2);
+        var backgroundSprite = new cc.Sprite();
+        backgroundSprite.initWithFile(Resources.helloBG_png);
+        backgroundSprite.setAnchorPoint(0, 0);
+        backgroundSprite.setPosition(0, 0);
+        backgroundSprite.scaleX = winsize.width / backgroundSprite.getContentSize().width;
+        backgroundSprite.scaleY = winsize.height / backgroundSprite.getContentSize().height;
+        backgroundSprite.setPosition(centerpos);
+        this.addChild(backgroundSprite);
 
-        //4. create a background image and set it's position at the center of the screen
-        var spritebg = new cc.Sprite();
-
-        //var newWidth = 300;
-        //var newHeight = 200;
-        //
-        //var startWidth = spritebg.getContentSize().width;
-        //var startHeight = spritebg.getContentSize().height;
-        //
-        //var newScaleX = newWidth/startWidth;
-        //var newScaleY = newHeight/startHeight;
-        //
-        //spritebg.scaleX = newScaleX;
-        //spritebg.scaleY = newScaleY;
-        spritebg.initWithFile(Resources.helloBG_png);
-        spritebg.setAnchorPoint(0, 0);
-        spritebg.setPosition(0, 0);
-        spritebg.scaleX = winsize.width / spritebg.getContentSize().width;
-        spritebg.scaleY = winsize.height / spritebg.getContentSize().height;
-
-        setInterval(function () {
-            cc.log("asd");
-        }, 1000);
-
-        //spritebg.setScaleX(winsize.width / spritebg.getContentSize().width);
-        //spritebg.setScaleY(winsize.height / spritebg.getContentSize().height);
-        //spritebg.setPositionX(40);
-        //spritebg.setPositionY(50);
-        spritebg.setPosition(centerpos);
-        this.addChild(spritebg);
-
-        //5.
-        //cc.MenuItemFont.setFontSize(60);
         var startNSprite = new cc.Sprite();
         startNSprite.initWithFile(Resources.start_n_png);
         var startSSprite = new cc.Sprite();
         startSSprite.initWithFile(Resources.start_s_png);
-
-        //6.create a menu and assign onPlay event callback to it
         var menuItemPlay = cc.MenuItemSprite.create(startNSprite, startSSprite, this.play, this);
+        menuItemPlay.setPosition(centerpos);
         var menu = cc.Menu.create(menuItemPlay);
         menu.setPosition(centerpos);
-
-        //menu.setAnchorPoint(0,0);
-        //menu.setPosition(0, 0);
         this.addChild(menu);
     }
     return MenuLayer;
